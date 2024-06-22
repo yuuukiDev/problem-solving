6190fbd08411cc7b03dd873013c4cc266a125e50
yuuukiDev <mohamedemadeldin34@gmail.com>
2024-06-22 08:37:18 +0300

    updated valid anagram with better algorithm

--- Leetcode/Easy/242. Valid Anagram/solution.php
+++ Leetcode/Easy/242. Valid Anagram/solution.php
@@ -9,18 +9,17 @@ class Solution {
         if(strlen($s) !== strlen($t))
             return false;
     
-        $countS = [];
-        $countT = [];
+        $count = [];
         
         for($i = 0; $i < strlen($s); $i++) {
             $charS = $s[$i];
             $charT = $t[$i];
     
-            $countS[$charS] = ($countS[$charS] ?? 0) + 1;
-            $countT[$charT] = ($countT[$charT] ?? 0) + 1;
+            $count[$charS] = ($count[$charS] ?? 0) + 1;
+            $count[$charT] = ($count[$charT] ?? 0) - 1;
         }
-        foreach($countS as $char => $count) {
-            if(($countT[$char] ?? 0) !== $count)
+        foreach($count as $char) {
+            if($char !== 0)
                 return false;
         }
         return true;
