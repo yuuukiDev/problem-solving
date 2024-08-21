736aa5c7733cb6d901ebebad0a03fa2bc4c9028e
yuuukiDev <mohamedemadeldin34@gmail.com>
2024-08-21 14:32:12 +0300

    valid anagram solved

---
+++ Leetcode/Easy/242. Valid Anagram/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapS, mapT = {}, {}
+        for i in range(len(s)):
+            mapS[s[i]] = 1 + mapS.get(s[i], 0)
+            mapT[t[i]] = 1 + mapT.get(t[i], 0)
+        return mapS == mapT
+                
+        
